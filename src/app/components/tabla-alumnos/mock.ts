ec814b4068859ab069f4974c928693b9c944499f
Lucas Gastón <lucasgastonvillafane@gmail.com>
2024-07-28 13:06:43 -0300

    ListaAlumno-AngularTS

---
+++ src/app/components/tabla-alumnos/mock.ts
@@ -0,0 +1,17 @@
+export const alumnos = [
+    {
+        nombre: 'Gaston',
+        apellido: 'Lopez',
+        promedio: 8,
+    },
+    {
+        nombre: 'Elias',
+        apellido: 'Monaco',
+        promedio: 9,
+    },
+    {
+        nombre: 'Matias',
+        apellido: 'Monaco',
+        promedio: 7,
+    }
+]
---
+++ src/app/components/tabla-cursos/mock.ts
@@ -0,0 +1,17 @@
+export const cursos = [
+    {
+        materia: 'JavaScript',
+        dificultad: 'Medio',
+        requiere: 'HTML Y CSS',
+    },
+    {
+        materia: 'React',
+        dificultad: 'Dificil',
+        requiere: 'JavaScript',
+    },
+    {
+        materia: 'Angular',
+        dificultad: 'Dificil',
+        requiere: 'JavaScript y TypeScript',
+    }
+]
